d9ce708953694b2d0ba1a53809913da9d7433bb8
evanrich2404 <evanrich2404@gmail.com>
2024-03-05 22:18:52 -0600

    did work on getting rootquery more setup and tested, fixed issues with schema not being imported correctly because of typos, and more

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -5,6 +5,21 @@ const {
   GraphQLSchema
 } = require('graphql');
 
+const tasks = [
+  {
+    id : '1',
+    title: 'Create your first webpage',
+    weight: 1,
+    description: 'Create your first HTML file 0-index.html with: - Add the doctype on the first line (without any comment) - After the doctype, open and close a html tag Open your file in your browser (the page should be blank)'
+  },
+  {
+    id: '2',
+    title: 'Structure your webpage',
+    weight: 1,
+    description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty in this order)'
+  }
+];
+
 const TaskType = new GraphQLObjectType({
   name: 'Task',
   fields: {
@@ -20,12 +35,14 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     task: {
       type: TaskType,
-    args: { id: { type: GraphQLString } },
-  resolve(parent, args) {
-    // empty function for now
-    }}
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        // Use the args.id to find and return the task from the tasks array
+        return tasks.find(task => task.id === args.id);
+      }
+    }
   }
-})
+});
 
 const schema = new GraphQLSchema({ query: RootQuery });
 module.exports = { schema };
